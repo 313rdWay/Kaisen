de852fcf069c549d2c4224c34d01a0490c3a9fb0
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-03-23 19:54:45 -0400

    initital commit

---
+++ Kaisen/Views/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  Kaisen
+//
+//  Created by Davaughn Williams on 3/23/25.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
