5a6827d90c2013ae727f0389349e01e9eee4c9e3
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-03-23 19:44:11 -0400

    Initial Commit

---
+++ Kaisen/KaisenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaisenApp.swift
+//  Kaisen
+//
+//  Created by Davaughn Williams on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct KaisenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KaisenTests/KaisenTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaisenTests.swift
+//  KaisenTests
+//
+//  Created by Davaughn Williams on 3/23/25.
+//
+
+import Testing
+@testable import Kaisen
+
+struct KaisenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
